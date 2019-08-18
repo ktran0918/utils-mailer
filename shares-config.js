e0316f522af72b774df81f0e7e134c97143d45be
Khoa Tran <trankho@oregonstate.edu>
2019-08-18 16:42:33 -0700

    Utilities are no longer passed in as command line arguments: - Utitlies info are now stored in shares-config.js (renamed from sharers.js). - Add .gitignore

---
+++ shares-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  utilities: [
+    {
+      name: "",
+      cost: 0
+    },
+    {
+      name: "",
+      cost: 0
+    }
+  ],
+
+  sharers: [
+    {
+      name: "",
+      email: "",
+      portion: 0.25
+    },
+    {
+      name: "",
+      email: "",
+      portion: 0.25
+    },
+    {
+      name: "",
+      email: "",
+      portion: 0.25
+    },
+    {
+      name: "",
+      email: "",
+      portion: 0.25
+    }
+  ]
+};
--- utils-mailer.js
+++ utils-mailer.js
@@ -1,40 +1,42 @@
-const nodeMailer = require('nodemailer');
-const handlebars = require('handlebars');
-const fs = require('fs');
+const nodeMailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
 
-const mailConfig = require('./mail-config');
-const sharers = require('./sharers');
+const mailConfig = require("./mail-config");
+const sharesConfig = require("./shares-config");
+const utils = sharesConfig.utilities;
+const sharers = sharesConfig.sharers;
 const date = new Date();
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-const utilsAmounts = process.argv.slice(2);
+//const utilsAmounts = process.argv.slice(2);
 
 const monthNames = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
 ];
 
-// Bill Period: [last month] - [this month]
+// Bill Period: [last month] [em dash] [this month]
 // \u2013: en dash
 const billPeriod = `${monthNames[date.getMonth() - 1]} \u2013 ${
   monthNames[date.getMonth()]
-  }`;
+}`;
 const mailSubject = `Utilities Share in ${billPeriod}`;
 
 var utilsTotal = 0;
 
 const readMailTemplate = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile('mail-template/mail-template.handlebars', (err, data) => {
+    fs.readFile("mail-template/mail-template.handlebars", (err, data) => {
       err ? reject(err) : resolve(data);
     });
   });
@@ -44,13 +46,14 @@ calculateShares();
 sendEmail();
 
 function calculateShares() {
-  for (let amount of utilsAmounts) {
-    utilsTotal += Number(amount);
+  let i;
+  for (i = 0; i < utils.length; i++) {
+    utilsTotal += utils[i].cost;
   }
   utilsTotal = utilsTotal.toFixed(2);
   console.log(`Total utilities amount: ${utilsTotal}`);
 
-  for (let i = 0; i < sharers.length; i++) {
+  for (i = 0; i < sharers.length; i++) {
     let shareAmount = (utilsTotal * sharers[i].portion).toFixed(2);
     sharers[i].shareAmount = shareAmount;
     console.log(`${sharers[i].name}'s share: ${sharers[i].shareAmount}`);
@@ -59,9 +62,10 @@ function calculateShares() {
 
 function createEmail(mailTemplate, recipient) {
   let placeholders = {
-    senderName: 'Khoa',
+    senderName: "Khoa",
     recipient: recipient,
     billPeriod: billPeriod,
+    utilities: utils,
     utilsTotal: utilsTotal,
     sharers: sharers,
     lastDay: dateWithOrdinal(lastDay.getDate())
@@ -90,7 +94,7 @@ async function sendEmail() {
       if (error) {
         console.log(error);
       } else {
-        console.log('Email sent: ', sendInfo.accepted, sendInfo.response);
+        console.log("Email sent: ", sendInfo.accepted, sendInfo.response);
       }
     });
   }
@@ -99,17 +103,17 @@ async function sendEmail() {
 function dateWithOrdinal(d) {
   let ordinal;
   if (d > 3 && d < 21) {
-    ordinal = 'th';
+    ordinal = "th";
   } else {
     switch (d % 10) {
       case 1:
-        ordinal = 'st';
+        ordinal = "st";
         break;
       case 2:
-        ordinal = 'nd';
+        ordinal = "nd";
         break;
       case 3:
-        ordinal = 'rd';
+        ordinal = "rd";
         break;
     }
   }
