5421396052f43882d2275e1a9f0024253583c9ad
Khoa Tran <k.tran.0918@outlook.com>
2019-11-15 21:35:59 -0800

    bring project up to date

--- utils-mailer.js
+++ utils-mailer.js
@@ -1,56 +1,58 @@
-const nodeMailer = require("nodemailer");
-const handlebars = require("handlebars");
-const fs = require("fs");
+const nodeMailer = require('nodemailer');
+const handlebars = require('handlebars');
+const fs = require('fs');
 
-const mailConfig = require("./mail-config");
-const sharesConfig = require("./shares-config");
+// import nodemailer configurations
+const mailConfig = require('./mail-config');
+// import utilities shares data
+const sharesConfig = require('./shares-config');
 const utils = sharesConfig.utilities;
 const sharers = sharesConfig.sharers;
+
 const date = new Date();
+// Setting the dayValue 0 will set the date to the last day of the previous month
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-//const utilsAmounts = process.argv.slice(2);
 
 const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
 ];
 
-// Bill Period: [last month] [em dash] [this month]
 // \u2013: en dash
 const billPeriod = `${monthNames[date.getMonth() - 1]} \u2013 ${
   monthNames[date.getMonth()]
-}`;
+  }`;
 const mailSubject = `Utilities Share in ${billPeriod}`;
 
 var utilsTotal = 0;
 
-const readMailTemplate = () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile("mail-template/mail-template.handlebars", (err, data) => {
-      err ? reject(err) : resolve(data);
-    });
-  });
-};
-
+/**
+ * Program execution
+ */
 calculateShares();
 sendEmail();
 
+/**
+ * Function definitions
+ */
 function calculateShares() {
   let i;
   for (i = 0; i < utils.length; i++) {
+    // set precision of utility's cost to 2 decimal places for legibility
+    utils[i].cost = utils[i].cost.toFixed(2);
     utilsTotal += utils[i].cost;
   }
-  utilsTotal = utilsTotal.toFixed(2);
+  utilsTotal = Number(utilsTotal).toFixed(2);
   console.log(`Total utilities amount: ${utilsTotal}`);
 
   for (i = 0; i < sharers.length; i++) {
@@ -62,13 +64,13 @@ function calculateShares() {
 
 function createEmail(mailTemplate, recipient) {
   let placeholders = {
-    senderName: "Khoa",
+    senderName: 'Khoa',
     recipient: recipient,
     billPeriod: billPeriod,
     utilities: utils,
     utilsTotal: utilsTotal,
     sharers: sharers,
-    lastDay: dateWithOrdinal(lastDay.getDate())
+    lastDay: `${monthNames[date.getMonth()]} ${dateWithOrdinal(lastDay.getDate())}`
   };
 
   return mailTemplate(placeholders);
@@ -76,7 +78,7 @@ function createEmail(mailTemplate, recipient) {
 
 async function sendEmail() {
   mail = await readMailTemplate();
-  let mailTemplate = handlebars.compile(mail.toString());
+  let mailTemplate = handlebars.compile(mail);
 
   for (let i = 0; i < sharers.length; i++) {
     mailToSend = createEmail(mailTemplate, sharers[i]);
@@ -94,26 +96,46 @@ async function sendEmail() {
       if (error) {
         console.log(error);
       } else {
-        console.log("Email sent: ", sendInfo.accepted, sendInfo.response);
+        console.log('Email sent: ', sendInfo.accepted, sendInfo.response);
       }
     });
   }
 }
 
+/**
+ * read "mail-template.handlebars" file
+ *
+ * @returns {Promise} result of file read
+ */
+function readMailTemplate() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(
+      'mail-template/mail-template.handlebars',
+      'utf-8',
+      (err, data) => {
+        err ? reject(err) : resolve(data);
+      }
+    );
+  });
+}
+
 function dateWithOrdinal(d) {
   let ordinal;
   if (d > 3 && d < 21) {
-    ordinal = "th";
+    ordinal = 'th';
   } else {
     switch (d % 10) {
       case 1:
-        ordinal = "st";
+        ordinal = 'st';
         break;
       case 2:
-        ordinal = "nd";
+        ordinal = 'nd';
         break;
       case 3:
-        ordinal = "rd";
+        ordinal = 'rd';
+        break;
+      default:
+        ordinal = 'th';
         break;
     }
   }
